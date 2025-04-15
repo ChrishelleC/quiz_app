197afb75b1c7cd138de12f85afebea762aa16712
ChrishelleC <culmerchrishelle12@gmail.com>
2025-04-14 21:28:08 -0400

    updated files and yaml

--- lib/question.dart
+++ lib/question.dart
@@ -1,23 +1,27 @@
+import 'package:html_unescape/html_unescape.dart';
+
 class Question {
-final String question;
-final List<String> options;
-final String correctAnswer;
-Question({
-required this.question,
-required this.options,
-required this.correctAnswer,
-});
-factory Question.fromJson(Map<String, dynamic> json) {
-// Decode options by combining incorrect answers with the correct
-answer and shuffling them.
-List<String> options =
-List<String>.from(json['incorrect_answers']);
-options.add(json['correct_answer']);
-options.shuffle();
-return Question(
-question: json['question'],
-options: options,
-correctAnswer: json['correct_answer'],
-);
-}
+  final String question;
+  final List<String> options;
+  final String correctAnswer;
+
+  Question({
+    required this.question,
+    required this.options,
+    required this.correctAnswer,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> json) {
+    var unescape = HtmlUnescape();
+    List<String> options = List<String>.from(json['incorrect_answers']);
+    String correct = json['correct_answer'];
+    options.add(correct);
+    options.shuffle();
+
+    return Question(
+      question: unescape.convert(json['question']),
+      options: options.map((e) => unescape.convert(e)).toList(),
+      correctAnswer: unescape.convert(correct),
+    );
+  }
 }
